918bfaa5decebb955b50d69ec29e79e65585e84b
Alexey_Berezin <alexey_berezin@epam.com>
2018-04-13 16:39:19 +0300

    Add changes for Heroku

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const socksv5 = require('socksv5')
 
-const DOMAIN = 'localhost'
-const PORT = 1080
+const DOMAIN = 'socks5-telegram-server.herokuapp.com'
+const PORT = 9002
 
 const server = socksv5.createServer((_, resolve) => resolve())
 server.listen(PORT, DOMAIN, () => console.log(`Listen: ${DOMAIN}:${PORT}`))
 server.useAuth(socksv5.auth.UserPassword((username, password, cb) => {
-  console.log(`Connected a new user: ${username}`)
+  console.log(`Connected a new user: ${username} ${password}`)
   cb(true)
 }))
