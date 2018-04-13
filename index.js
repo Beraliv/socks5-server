18e24484bce0a75a54c4fff61980da45d90d0e3a
Alexey_Berezin <alexey_berezin@epam.com>
2018-04-13 16:49:09 +0300

    Remove domain for Heroku

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const socksv5 = require('socksv5')
 
-const DOMAIN = '54.243.101.177'
+console.log(process.env.IP)
+
 const PORT = 9002
 
 const server = socksv5.createServer((_, resolve) => resolve())
-server.listen(PORT, DOMAIN, () => console.log(`Listen: ${DOMAIN}:${PORT}`))
+server.listen(PORT, () => console.log(`Listen: localhost:${PORT}`))
 server.useAuth(socksv5.auth.UserPassword((username, password, cb) => {
   console.log(`Connected a new user: ${username} ${password}`)
   cb(true)
