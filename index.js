7c42ee99da150193c9d426e28393e1e6669b6d3e
Alexey_Berezin <alexey_berezin@epam.com>
2018-04-16 11:23:29 +0300

    Remove QUOTAGUARDSTATIC_URL for Heroku

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const socksv5 = require('socksv5')
 
-console.log(process.env.QUOTAGUARDSTATIC_URL)
-
-const PORT = 8080
-
+const PORT = 1080
 const server = socksv5.createServer((_, resolve) => resolve())
+
 server.listen(PORT, () => console.log(`Listen: localhost:${PORT}`))
 server.useAuth(socksv5.auth.UserPassword((username, password, cb) => {
   console.log(`Connected a new user: ${username} ${password}`)
